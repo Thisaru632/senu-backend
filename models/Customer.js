6712dfb6ded3ccb78bcc2c610eda880657f6fcfe
Thisaru632 <thisarudilhara2@gmail.com>
2026-03-10 09:52:59 +0530

    feat: Implement Customer Mongoose model with schema definition, password hashing, and comparison methods.

--- models/Customer.js
+++ models/Customer.js
@@ -52,18 +52,13 @@ const customerSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash password before saving
-customerSchema.pre('save', async function (next) {
+customerSchema.pre('save', async function () {
     if (!this.isModified('password')) {
-        return next();
+        return;
     }
 
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (err) {
-        next(err);
-    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Method to compare password
